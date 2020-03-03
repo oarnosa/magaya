e195f353d65a22a9ad0609057b668ba348e61215
Orlando Arnosa <oarnosa@gmail.com>
2020-03-03 10:14:19 -0500

    Create development branch, include global typography styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <h1>Magaya</h1>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ex
+        perspiciatis ad consequuntur atque quibusdam, consectetur dicta nobis
+        sed voluptatum eligendi voluptate officia omnis necessitatibus sit
+        voluptates placeat obcaecati ullam voluptatem, quo eum vitae. Porro
+        eius, tempora praesentium qui ad dignissimos nihil perspiciatis eaque
+        voluptatibus! Assumenda error excepturi enim ut!
+      </p>
+      <a href="_blank">link</a>
     </div>
   );
 }
