4188b66b1467c5de0ad6ff7ac19027c04255587e
Orlando Arnosa <oarnosa@gmail.com>
2020-03-03 14:08:02 -0500

    Create content pages and their routes, remove padding from global styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Home from "./pages/home/home.component";
+import Solutions from "./pages/solutions/solutions.component";
+import About from "./pages/about/about.component";
+import Contact from "./pages/contact/contact.component";
+
 import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <h4>Heading 4</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ex
-        perspiciatis ad consequuntur atque quibusdam, consectetur dicta nobis
-        sed voluptatum eligendi voluptate officia omnis necessitatibus sit
-        voluptates placeat obcaecati ullam voluptatem, quo eum vitae. Porro
-        eius, tempora praesentium qui ad dignissimos nihil perspiciatis eaque
-        voluptatibus! Assumenda error excepturi enim ut!
-      </p>
-      <a href="_blank">link</a>
-    </div>
+    <Router>
+      <div className="wrapper">
+        <Route exact path="/" component={Home} />
+        <Route path="/solutions" component={Solutions} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/solutions/solutions.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Solutions = () => (
+  <div>
+    <h1>Solutions</h1>
+  </div>
+);
+
+export default Solutions;
