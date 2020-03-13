6f22dc4386abb31ad9a6b7c592ab6a54205ff08a
Orlando Arnosa <oarnosa@gmail.com>
2020-03-12 20:38:49 -0400

    Change viewport sizing for larger screens across all pages

--- src/components/menu/menu.component.jsx
+++ src/components/menu/menu.component.jsx
@@ -1,27 +1,20 @@
 import React from "react";
-import Reveal from "react-reveal";
 import { Link } from "react-router-dom";
 
 import "./menu.styles.scss";
 
 const Menu = () => (
-  <Reveal effect="fadeIn">
-    <div className="menu">
-      <Link
-        to="/solutions"
-        className="menu__option"
-        aria-label="Solutions Page"
-      >
-        Solutions
-      </Link>
-      <Link to="/about" className="menu__option" aria-label="About Page">
-        About Us
-      </Link>
-      <Link to="/contact" className="menu__option" aria-label="Contact Page">
-        Contact
-      </Link>
-    </div>
-  </Reveal>
+  <div className="menu">
+    <Link to="/solutions" className="menu__option" aria-label="Solutions Page">
+      Solutions
+    </Link>
+    <Link to="/about" className="menu__option" aria-label="About Page">
+      About Us
+    </Link>
+    <Link to="/contact" className="menu__option" aria-label="Contact Page">
+      Contact
+    </Link>
+  </div>
 );
 
 export default Menu;
